de6e27c103228ab56db1994c90929052f2a005fe
Orlando Saldana <orlando.saldana@upr.edu>
2021-07-10 15:27:59 -0400

    Created the three classes to represent the network

---
+++ src/Bus.py
@@ -0,0 +1,37 @@
+class Bus:
+    def __init(self, id, load):
+        self.ID = id
+        self.load = load
+
+    ID = None
+    load = 0
+
+
+class Line:
+    def __init(self, id, inputbus, outputbus, cap):
+        self.ID = id
+        self.inputBus = inputbus
+        self.outputBus = outputbus
+        self.capacity = cap
+
+    ID = None
+    inputBus = None
+    outputBus = None
+    susceptance = 0
+    capacity = None
+
+
+class Generator:
+    def __init__(self, id, locationbus, outputmax, outputmin, cost):
+        self.ID = id
+        self.locationBus = locationbus
+        self.outputMaximum = outputmax
+        self.outputMinimum = outputmin
+        self.cost = cost
+
+    ID = None
+    locationBus = None
+    outputMaximum = 0
+    outputMinimum = 0
+    cost = 0
+
