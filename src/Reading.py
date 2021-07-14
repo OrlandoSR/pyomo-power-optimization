5f5750f4b7d27b2d2e08c7416ec8fe102377a1d3
Orlando Saldana <orlando.saldana@upr.edu>
2021-07-13 20:32:50 -0400

    Added 4bus examples

--- src/Reading.py
+++ src/Reading.py
@@ -1,10 +1,12 @@
 # Code for creating the lists given to Reusable.py from text files
 # \t is tab and \n is new line
+from io import StringIO
+import csv
 from Variables import *
 
-path_to_line = "../res/3bus/line.txt"
-path_to_bus = "../res/3bus/bus.txt"
-path_to_generator = "../res/3bus/gen.txt"
+path_to_line = "../res/4Bus/line.txt"
+path_to_bus = "../res/4Bus/bus.txt"
+path_to_generator = "../res/4Bus/gen.txt"
 
 with open(path_to_line) as f_line:
     linecontent = f_line.readlines()
@@ -33,8 +35,64 @@ gencontent = cleanList(gencontent, "\t", ",")
 linecontent = cleanList(linecontent, "\n", "")
 linecontent = cleanList(linecontent, "\t", ",")
 
-print(buscontent)
-print(linecontent)
-print(gencontent)
-
 # Take the cleaned list of strings and create an appropiate object for each
+# for each individual string in the list split at the
+def buildBusList(bclist):
+    busList = []
+    paramList = []
+
+    for i in bclist:
+        f = StringIO(i)
+        reader = csv.reader(f, delimiter=',')
+        for j in reader:
+            paramList.append(j)
+
+    for i in paramList:
+         busList.append(Bus(i[0], float(i[1])))
+
+    return busList
+
+def buildLineList(llist):
+    lineList = []
+    paramList = []
+
+    for i in llist:
+        f = StringIO(i)
+        reader = csv.reader(f, delimiter=',')
+        for j in reader:
+            paramList.append(j)
+
+    for i in paramList:
+        lineList.append(Line(i[0], i[1], i[2], float(i[5]), float(i[3])))
+
+    return lineList
+
+
+def buildGenList(glist):
+    genList = []
+    paramList = []
+
+    for i in glist:
+        f = StringIO(i)
+        reader = csv.reader(f, delimiter=',')
+        for j in reader:
+            paramList.append(j)
+
+    for i in paramList:
+        genList.append(Generator(i[0], i[1], float(i[2]), float(i[3]), float(i[4])))
+
+    return genList
+
+
+busList = buildBusList(buscontent)
+lineList = buildLineList(linecontent)
+genList = buildGenList(gencontent)
+
+for i in range(len(busList)):
+    print(f"Bus: ID = {busList[i].ID}, Load = {busList[i].load}")
+
+for i in range(len(lineList)):
+    print(f"Line: ID = {lineList[i].ID}, Input bus = {lineList[i].inputBus}, Output bus = {lineList[i].outputBus}, Capacity = {lineList[i].capacity}, Susceptance = {lineList[i].susceptance}")
+
+for i in range(len(genList)):
+    print(f"Generator: ID = {genList[i].ID}, Location Bus = {genList[i].locationBus}, Cost = {genList[i].cost}, Max = {genList[i].outputMaximum}, Min = {genList[i].outputMinimum}")
